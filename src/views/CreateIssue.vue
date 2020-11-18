1b582631776e44e5f8a094bf6db9a7fbcf575b43
innegime <NingDongOne@163.com>
2020-11-18 08:51:32 +0800

    新建 CreateIssue.vue

---
+++ src/views/CreateIssue.vue
@@ -0,0 +1,203 @@
+<template>
+  <div class="CIcontent">
+    <el-form
+      :model="ruleForm"
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+      style="align-self: center"
+    >
+    <el-row>
+      <el-col :span="12"
+        ><el-form-item label="Issue题目" prop="name">
+          <el-input v-model="ruleForm.name"></el-input> </el-form-item
+      ></el-col>
+      <el-col :span="12">
+        <el-form-item label="Issue No." prop="id">
+          <el-input v-model="ruleForm.id"></el-input> </el-form-item
+      ></el-col>
+    </el-row>
+      <el-row>
+        <el-col :span="12"
+        ><el-form-item label="指派人" prop="person">
+          <el-input v-model="ruleForm.person"></el-input> </el-form-item
+      ></el-col>
+      <el-col :span="12">
+        <el-form-item label="影响版本" prop="version" :inline="true">
+          <el-input v-model="ruleForm.version"></el-input> </el-form-item
+      ></el-col>
+      </el-row>
+      
+      <el-form-item label="创建时间" required>
+        <el-col :span="11">
+          <el-form-item prop="date1">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="ruleForm.date1"
+              style="width: 100%"
+            ></el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col class="line" :span="2">-</el-col>
+        <el-col :span="11">
+          <el-form-item prop="date2">
+            <el-time-picker
+              placeholder="选择时间"
+              v-model="ruleForm.date2"
+              style="width: 100%"
+            ></el-time-picker>
+          </el-form-item>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="修改时间" required>
+        <el-col :span="11">
+          <el-form-item prop="date1">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="ruleForm.date1"
+              style="width: 100%"
+            ></el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col class="line" :span="2">-</el-col>
+        <el-col :span="11">
+          <el-form-item prop="date2">
+            <el-time-picker
+              placeholder="选择时间"
+              v-model="ruleForm.date2"
+              style="width: 100%"
+            ></el-time-picker>
+          </el-form-item>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="完成时间" required>
+        <el-col :span="11">
+          <el-form-item prop="date1">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="ruleForm.date1"
+              style="width: 100%"
+            ></el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col class="line" :span="2">-</el-col>
+        <el-col :span="11">
+          <el-form-item prop="date2">
+            <el-time-picker
+              placeholder="选择时间"
+              v-model="ruleForm.date2"
+              style="width: 100%"
+            ></el-time-picker>
+          </el-form-item>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="步骤重现" prop="desc">
+        <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+      </el-form-item>
+      <el-form-item label="Issue等级" prop="region">
+        <el-select v-model="ruleForm.region" placeholder="请选择Issue等级">
+          <el-option label="最高" value="highest"></el-option>
+          <el-option label="较高" value="hight"></el-option>
+          <el-option label="一般" value="normal"></el-option>
+          <el-option label="低" value="low"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >立即创建</el-button
+        >
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateIssue",
+  data() {
+    return {
+      ruleForm: {
+        name: "",
+        id: "",
+        person: "",
+        version: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+      rules: {
+        name: [
+          { required: true, message: "请输入ISSUE题目", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+        region: [
+          { required: true, message: "请填写步骤重现", trigger: "change" },
+        ],
+        date1: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择日期",
+            trigger: "change",
+          },
+        ],
+        date2: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择时间",
+            trigger: "change",
+          },
+        ],
+        type: [
+          {
+            type: "array",
+            required: true,
+            message: "请至少选择一个活动性质",
+            trigger: "change",
+          },
+        ],
+        resource: [
+          { required: true, message: "请选择活动资源", trigger: "change" },
+        ],
+        desc: [{ required: true, message: "请填写步骤重现", trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
+</script>
+
+
+<style>
+.CIcontent {
+  
+  height: 100%;
+  width: 80%;
+  margin: 50px auto;
+
+}
+</style>
